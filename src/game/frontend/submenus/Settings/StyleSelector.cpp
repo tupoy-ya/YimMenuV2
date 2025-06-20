be762b761afe3a3d150c8559b6e3346036fed85c
DeadlineEm <jownsall@gmail.com>
2025-06-20 18:24:54 -0400

    (Improvement) Custom UI Styles (#503)
    * (feat) Bypass Casino Bans for all Tables/Slots
    
    * (feat) Bypass Casino Bans for all Tables/Slots (Fix)
    
    * (feat) Bypass Casino Bans (Refactor) added to a Tools section for global use instead of being directly in the slots section.
    
    * (feat) Casino Lucky Wheel
    
    * (feat) Casino Lucky Wheel Prizes
    
    * Refactor Casino Lucky Wheel
    
    * (feat) Refactor - Added Lucky Wheel Bypass
    
    * (feat) Removed Lucky Wheel Prize/Autospin for now.
    
    * (feat) Casino Rig Options - Removed Lucky Wheel and Slots Autospin (temporarily)
    Added notice on risk to bypassing the bans/cooldowns.
    
    * Removed Cyrillic due to crashing/bug issues
    Restyled UI to use FontAwesome
    
    * UI Updates
    
    * fix
    
    * UI alterations
    
    * Removed Cyrillic due to Crashing/Bugs
    Added FULL FontAwesome Icon support
    Altered UI with floating navigation.
    
    * Fix
    
    * (Improvement) UI Restyling with floating Navigation, Menu resizing/moving and full FontAwesome icon support.
    
    * (Fix) Explosive Ammo not working in SP + more
    
    * (Fix) Explosive Ammo
    - Added combo box for selecting different ammo types
    - Added magic_enum for dynamic Explosion type listings using ExplosionType.hpp
    
    * (Improved) Explosive Ammo with damage scale modifier and camera shake modifier
    In Progress - Weapon Damage modifier
    
    * (feat) Added Weapon Damage Modifier
    
    * Fixes:
    Moved Melee damage modifier from WeaponDamage to its own option
    Fixes reporting PAD natives in UIManager and restored them in GUI with proper impl
    Removed OnEnable from ExplosiveAmmo
    
    * Casino script work
    
    * Alterations:
    
    Removed casino bypass cooldown (per request)
    Removed magic_enum
    Added array of explosion types as an alternative
    
    * moved m_ShowContentWindow from public to private
    
    * Fixed Weapon Damage description
    Passed ExplosionTypeArray directly
    
    * Altered UI Colors:
    Purple to Green as per request
    
    * Alter Melee Damage (test)
    
    * Improved Melee Damage:
    Now applies force more accurately than before.
    NOTE: Does not seem to apply damage to bare fists?
    
    * (Improvement)
    Weapon Damage now applies to weapons
    Explosive damage no longer happens during melee actions
    Melee damage improved with better damage scaling and applies force more accurately.
    
    - Added WEAPON_UNARMED to Weapons.hpp
    
    * (Improvement) Added Selectable UI styles
    - Switch between Classic and Modern UI
    - Easily add custom styles by creating a new folder inside core/frontend/manager/styles and adding your style.cpp
    - Automatic list population for newly created styles without needing to edit multiple files.
    
    * (UI) Added ModernV (Vertical) style
    - Added ability to drag the navigation bar for both Modern and ModernV styles
    (Click/Drag slightly to the right of the bar, for Modern, right after the Debug button and for ModernV to the right of the button column, the drag button is INVISIBLE)
    
    * Applied proper default positioning to ModernV style
    
    * (UI) Fixes to the StyleSelector:
    - Removed need for StyleManager
    - Altered functionality instead of automatically generated list items it now requires extra code (as per request)
    - Altered theme files to reflect new changes.
    
    * Small Fix to UIManager
    
    * (UI) Fixes:
    - Added Themes.hpp for theme declarations
    - Removed declarations from UIManager files
    - Fixed a small issue with functions i goofed up
    - Added some comments for clarity when adding new themes
    
    * fix m_ActiveSubmenu
    
    * Fix missing ;

---
+++ src/game/frontend/submenus/Settings/StyleSelector.cpp
@@ -0,0 +1,52 @@
+﻿#include "StyleSelector.hpp"
+#include "core/commands/ListCommand.hpp"
+#include "core/frontend/manager/UIManager.hpp"
+
+namespace YimMenu
+{
+	static const std::vector<std::pair<int, const char*>> g_StyleOptions = {
+	    {0, "Classic"},
+	    {1, "Modern"},
+	    {2, "ModernV"},
+	};
+
+	class StyleSelector : public ListCommand
+	{
+	public:
+		using ListCommand::ListCommand;
+
+		void OnChange() override
+		{
+			int selectedIndex = GetState();
+
+			UITheme selectedTheme = UITheme::Classic; // Default Theme
+
+			// Set the selected theme based on the index, append when adding new themes
+			switch (selectedIndex)
+			{
+			case 0:
+				selectedTheme = UITheme::Classic;
+				break;
+			case 1:
+				selectedTheme = UITheme::Modern;
+				break;
+			case 2:
+				selectedTheme = UITheme::ModernV;
+				break;
+			default:
+				selectedTheme = UITheme::Classic;
+				break;
+			}
+		}
+	};
+
+	// Expose as global reference so other code can use it
+	static StyleSelector g_InternalStyleSelector{
+	    "styleselector",
+	    "UI Style",
+	    "Choose the UI style",
+	    g_StyleOptions,
+	    0};
+
+	ListCommand& g_StyleSelector = g_InternalStyleSelector;
+}
