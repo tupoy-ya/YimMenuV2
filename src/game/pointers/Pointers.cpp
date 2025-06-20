17f55916c85bd92181210c53c7b191372823cc0b
maybegreat48 <96936658+maybegreat48@users.noreply.github.com>
2025-06-20 14:33:28 -0400

    fix(anticheat): fix BattlEyeStatusUpdatePatch

--- src/game/pointers/Pointers.cpp
+++ src/game/pointers/Pointers.cpp
@@ -229,7 +229,16 @@ namespace YimMenu
 
 		constexpr auto battlEyeStatusUpdatePatchPtrn = Pattern<"80 B9 92 0A 00 00 01">("BattlEyeStatusUpdatePatch");
 		scanner.Add(battlEyeStatusUpdatePatchPtrn, [this](PointerCalculator ptr) {
-			BattlEyeStatusUpdatePatch = BytePatches::Add(ptr.Sub(0x26).As<std::uint8_t*>(), 0xC3);
+			BattlEyeStatusUpdatePatch = BytePatches::Add(ptr.As<void*>(), 
+				// since arxan obfuscated this subroutine, return mid-function instead
+				// TODO: this might break in a later update
+				std::to_array<std::uint8_t>({
+					0x48, 0x83, 0xC4, 0x38, // add rsp, 38h
+					0x5F,                   // pop rdi
+					0x5E,                   // pop rsi
+					0xC3                    // ret
+				})
+			);
 		});
 
 		constexpr auto writeNetArrayDataPtrn = Pattern<"0F 84 06 03 00 00 0F B6 83">("WriteNetArrayData");
