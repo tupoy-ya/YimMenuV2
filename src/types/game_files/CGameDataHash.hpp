b37b0018b6abecbc1847081ed1c7befdef490283
tupoy-ya <tupoy-ya@users.noreply.github.com>
2026-01-04 21:41:53 +0500

    feat: Update CGameDataHash to use Obf64.

--- src/types/game_files/CGameDataHash.hpp
+++ src/types/game_files/CGameDataHash.hpp
@@ -8,7 +8,7 @@ class CGameDataHash
 public:
 	bool m_IsJapaneseVersion;
 	std::array<rage::Obf32, 16> m_Data;
-	char m_GameSkeletonHash[0x18]; // Obf64
+	rage::Obf64 m_GameSkeletonHash;
 };
 static_assert(sizeof(CGameDataHash) == 0x11C);
 #pragma pack(pop)
